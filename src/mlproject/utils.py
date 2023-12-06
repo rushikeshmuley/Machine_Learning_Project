d9a8bcd78b65f7fb24076db3f16a5546c6a1246c
“rushikeshmuley <rushikeshmuley881998@gmail.com>
2023-12-06 11:30:40 +0530

    data transformation

--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -4,8 +4,8 @@ from src.mlproject.exception import CustomException
 from src.mlproject.logger import logging
 import pandas as pd
 from dotenv import load_dotenv
-# from sklearn.model_selection import GridSearchCV
-# from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
 import pymysql
 
 import pickle
@@ -39,3 +39,15 @@ def read_sql_data():
 
     except Exception as ex:
         raise CustomException(ex)
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
